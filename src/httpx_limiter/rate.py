5b02ed1be621a6edd3732ddcebf6526f2b009ff9
Moritz E. Beber <midnighter@posteo.net>
2025-08-21 10:46:18 +0200

    feat: add a method to retrieve the duration in microseconds

--- src/httpx_limiter/rate.py
+++ src/httpx_limiter/rate.py
@@ -43,3 +43,7 @@ class Rate(NamedTuple):
     def in_seconds(self) -> float:
         """Return the duration in unit seconds."""
         return self.duration.total_seconds()
+
+    def in_microseconds(self) -> int:
+        """Return the duration in unit microseconds."""
+        return int(self.duration.total_seconds() * 1_000_000)
